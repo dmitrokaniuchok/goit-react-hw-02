540d9977f852c7d795ca2f169da7a76cab2acb31
DmytroKaniuchok <kanyuchekdmitry@gmail.com>
2025-04-23 12:10:08 +0300

    start 02

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+import Description from "./components/Description.jsx";
+import Feedback from "./components/Feedback.jsx";
+import Options from "./components/Options.jsx";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+}
