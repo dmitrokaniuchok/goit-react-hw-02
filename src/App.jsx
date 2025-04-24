b48cf869ad134227cb08cacafe94720c973df9b7
DmytroKaniuchok <kanyuchekdmitry@gmail.com>
2025-04-24 13:32:16 +0300

    add jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,52 @@ import Feedback from "./components/Feedback.jsx";
 import Options from "./components/Options.jsx";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+export default function App() {
+  const [feedback, setFeedback] = useState(() => {
+    const saved = localStorage.getItem("feedbacks");
+    return saved ? JSON.parse(saved) : { good: 0, neutral: 0, bad: 0 };
+  });
+
+  useEffect(() => {
+    localStorage.setItem("feedbacks", JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((initialFeedback) => ({
+      ...initialFeedback,
+      [feedbackType]: initialFeedback[feedbackType] + 1,
+    }));
+  };
+
+  const reset = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
+  const totalPositive = totalFeedback
+    ? Math.round((feedback.good / totalFeedback) * 100)
+    : 0;
+
+  return (
+    <>
+      <Description />
+      <Options
+        leaveFeedback={updateFeedback}
+        reset={reset}
+        total={totalFeedback > 0}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          total={totalFeedback}
+          positive={totalPositive}
+        />
+      ) : (
+        <p>No feedback yet</p>
+      )}
+    </>
+  );
 }
